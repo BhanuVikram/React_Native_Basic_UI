9ff35e382fa145aa68d251cef9041ac4f0ad750a
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-05-03 10:11:28 -0700

    Feature: React Native initialization

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>React Native Basic UI</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
