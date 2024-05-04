12e822fe426f2449580366285de59b86d798b394
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-05-03 23:28:37 -0700

    Fix: File name imports

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
-import PurplePalette from "./src/components/PurplePalette";
-import GreenPalette from "./src/components/GreenPalette";
-import BluePalette from "./src/components/BluePalette";
+import PurplePalette from "./src/components/PurplePaletteComponent";
+import GreenPalette from "./src/components/GreenPaletteComponent";
+import BluePalette from "./src/components/BluePaletteComponent";
 
 export default function App() {
   return (
